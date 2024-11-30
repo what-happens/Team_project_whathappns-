bfff000ecc510830f88babef920c2d7030069de3
ddhsl <dnjs9199@naver.com>
2024-12-01 04:58:14 +0900

    🎨 Route 경로 추가 (test)

--- whathappen/src/App.js
+++ whathappen/src/App.js
@@ -1,8 +1,10 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import GlobalStyle from "./styles/GlobalStyle";
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 import { Provider } from "react-redux";
 import store from "./redux/store/store";
+import QuizLanding from "./pages/quizLanding";
+import QuizResult from "./pages/quizResult";
 
 function App() {
   return (
@@ -10,7 +12,10 @@ function App() {
       <BrowserRouter>
         <GlobalStyle />
         <Routes>
-          <Route path="/" element={<Home />}></Route>
+          {/* <Route path="/" element={<Home />}></Route> */}
+          <Route path="/" element={<Navigate to="/quiz" />} />
+          <Route path="/quiz-landing" element={<QuizLanding />} />
+          <Route path="/quiz" element={<QuizResult />} />
         </Routes>
       </BrowserRouter>
     </Provider>
