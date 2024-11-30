07ed093e6958b38c79314665d75a17a3ab981ba2
siwoon1602 <siwoon1602@gmail.com>
2024-11-30 23:49:18 +0900

    :lipstick: global style에 html 폰트사이즈, sr-only 추가

--- whathappen/src/styles/GlobalStyle.js
+++ whathappen/src/styles/GlobalStyle.js
@@ -2,7 +2,22 @@ import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 
 const GlobalStyle = createGlobalStyle`
-  ${reset}
+${reset}
+  html {
+    font-size: 10px;
+  }
+
+  .sr-only {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    margin: -1px;
+    padding: 0;
+    border: 0;
+    clip: rect(0, 0, 0, 0);
+    overflow: hidden;
+    white-space: nowrap;
+  }
 `;
 
 export default GlobalStyle;
