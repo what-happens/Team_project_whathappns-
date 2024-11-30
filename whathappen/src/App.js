ecc3512ee2cb034e79428620bb86ba7a07bd0afa
HwiGyoung Lee <ter0000@naver.com>
2024-11-30 18:59:06 +0900

    ✨ quiz 페이지 route 추가 : React router를 사용하여 quiz 페이지 경로 추가

--- whathappen/src/App.js
+++ whathappen/src/App.js
@@ -1,8 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GlobalStyle from "./styles/GlobalStyle";
-import Home from "./pages/Home";
 import { Provider } from "react-redux";
 import store from "./redux/store/store";
+import Quiz from "./pages/quiz/Quiz";
+import Home from "./pages/Home";
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <BrowserRouter>
         <GlobalStyle />
         <Routes>
-          <Route path="/" element={<Home />}></Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/quiz" element={<Quiz />} />
         </Routes>
       </BrowserRouter>
     </Provider>
