201285cd26c35a37e4e0cb75c717b9025809303d
HwiGyoung Lee <ter0000@naver.com>
2024-11-30 18:59:31 +0900

    💄 GlobalStyle 업데이트 : sr-only 클래스 추가

--- whathappen/src/styles/GlobalStyle.js
+++ whathappen/src/styles/GlobalStyle.js
@@ -3,9 +3,20 @@ import reset from "styled-reset";
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
-  
   html {
     font-size: 10px;
+  }
+
+  .sr-only {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    margin: -1px;
+    padding: 0;
+    border: 0;
+    clip: rect(0, 0, 0, 0);
+    overflow: hidden;
+    white-space: nowrap;
   }
 `;
 
