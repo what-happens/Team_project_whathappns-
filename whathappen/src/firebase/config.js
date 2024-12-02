746dec66a528d488533f8c0396c429ebf2c2e503
siwoon1602 <siwoon1602@gmail.com>
2024-12-02 10:32:17 +0900

    ✨ firebase 초기 셋팅

---
+++ whathappen/src/firebase/config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA4fu1LSz5QK-1JTYH9qfhR_7ngoBszsUg",
+  authDomain: "what-happen-867b5.firebaseapp.com",
+  projectId: "what-happen-867b5",
+  storageBucket: "what-happen-867b5.firebasestorage.app",
+  messagingSenderId: "775781696285",
+  appId: "1:775781696285:web:a5b28dddabcfe40c0957f4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
