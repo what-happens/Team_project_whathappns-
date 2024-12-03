48f50b106bd4144c26bf46aaf0ea24a42aace1c7
siwoon1602 <siwoon1602@gmail.com>
2024-12-03 14:11:19 +0900

    ✨ 로그인 state 정보 저장 기능 추가

--- whathappen/src/App.js
+++ whathappen/src/App.js
@@ -3,17 +3,47 @@ import GlobalStyle from "./styles/GlobalStyle";
 import Home from "./pages/Home";
 import Login from "./pages/login/Login";
 import Join from "./pages/join/Join";
-import { Provider } from "react-redux";
+import { Provider, useDispatch } from "react-redux";
 import store from "./redux/store/store";
 import QuizLanding from "./pages/quizLanding";
 import QuizResult from "./pages/quizResult";
 // import Quiz from "./pages/quiz/Quiz";
 import Exercise from "./pages/exercise/Exercise";
+import { useEffect } from "react";
+import { auth } from "./firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import { setUser, clearUser } from "./redux/authSlice";
+
+function AuthSync() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch(
+          setUser({
+            uid: user.uid,
+            email: user.email,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+          })
+        );
+      } else {
+        dispatch(clearUser());
+      }
+    });
+
+    return () => unsubscribe();
+  }, [dispatch]);
+
+  return null; // 렌더링 필요 없음
+}
 
 function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
+        <AuthSync />
         <GlobalStyle />
         <Routes>
           <Route path="/" element={<Home />}></Route>
