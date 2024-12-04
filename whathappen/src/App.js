0af8ecb527dfdb7e15a8fd79575456f92e2345f2
siwoon1602 <siwoon1602@gmail.com>
2024-12-04 10:30:21 +0900

    ✨ mypage route 수정 : 중첩 라우트로 이동

--- whathappen/src/App.js
+++ whathappen/src/App.js
@@ -32,7 +32,7 @@ function App() {
           <Route path="/study" element={<Study />}></Route>
           <Route path="/study-finish" element={<StudyFinish />}></Route>
           <Route element={<AuthHeader />}>
-            <Route path="/my-page" element={<MyPage />} />
+            <Route path="/mypage" element={<MyPage />} />
             <Route path="/review" element={<Review />} />
           </Route>
         </Routes>
