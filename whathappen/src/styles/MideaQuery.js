781067a1e6ab4dd8d084be01e5cdd6c6eca001f9
siwoon1602 <siwoon1602@gmail.com>
2024-12-05 13:31:20 +0900

    ✨ MideaQuery 함수 기능 추가

---
+++ whathappen/src/styles/MideaQuery.js
@@ -0,0 +1,18 @@
+import { css } from "styled-components";
+
+const sizes = {
+  large: 1200,
+  medium: 768,
+  small: 600,
+};
+
+const media = Object.entries(sizes).reduce((acc, [key, value]) => {
+  acc[key] = (first, ...interpolations) => css`
+    @media (max-width: ${value}px) {
+      ${css(first, ...interpolations)}
+    }
+  `;
+  return acc;
+}, {});
+
+export { media };
