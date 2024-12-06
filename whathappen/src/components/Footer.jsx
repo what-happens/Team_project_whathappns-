699be0ed81b35a87a63c31ca91e09d1b62be09cc
siwoon1602 <siwoon1602@gmail.com>
2024-12-06 16:32:54 +0900

    ♻️ footer 코드 리팩토링 -> 스타일드 컴포넌트 활용 및 프롭스타일 적용 dt dd 순서 변경

--- whathappen/src/components/Footer.jsx
+++ whathappen/src/components/Footer.jsx
@@ -1,21 +1,20 @@
-import React from "react";
-import { keyframes } from "styled-components";
 import styled from "styled-components";
+import PropTypes from "prop-types";
+import { keyframes } from "styled-components";
 
 const slideUp = keyframes`
   from {
-    opacity: 0;
-    transform: translateY(8rem);
+    transform: translateY(100%);
   }
   to {
-    opacity: 2;
     transform: translateY(0);
   }
 `;
+
 const FooterContent = styled.footer`
   width: 100%;
   box-sizing: border-box;
-  padding: 5.7rem 29rem 5.7rem 29rem;
+  padding: 5.7rem 29rem;
   background-color: #2e5dfe;
   display: flex;
   align-items: flex-end;
@@ -23,44 +22,77 @@ const FooterContent = styled.footer`
   color: white;
   animation: ${slideUp} 1s ease;
 `;
+
+const Title = styled.h3`
+  font-size: 4rem;
+  font-weight: 700;
+  margin-bottom: 2rem;
+`;
+
+const DescriptionList = styled.dl`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  font-size: 1.2rem;
+`;
+
+const DescriptionItem = styled.div`
+  dt {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    padding: 0;
+    margin: -1px;
+    overflow: hidden;
+    clip: rect(0, 0, 0, 0);
+    border: 0;
+  }
+`;
+
+const StyledAddress = styled.address`
+  font-style: normal;
+`;
+
 export default function Footer() {
   return (
     <FooterContent>
-      <address>
-        <h3
-          style={{ fontSize: "4rem", fontWeight: "700", marginBottom: "2rem" }}
-        >
-          이게되네?
-        </h3>
-        <dl
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            gap: "0.5rem",
-            fontSize: "1.2rem",
-          }}
-        >
-          <dt>
+      <StyledAddress>
+        <Title>이게되네?</Title>
+        <DescriptionList>
+          <DescriptionItem>
+            <dt>회사명</dt>
             <dd>주식회사 오르미</dd>
-          </dt>
-          <dt>
-            <dd> 대표 : 오르미</dd>
-          </dt>
-          <dt>
+          </DescriptionItem>
+          <DescriptionItem>
+            <dt>대표자</dt>
+            <dd>대표 : 오르미</dd>
+          </DescriptionItem>
+          <DescriptionItem>
+            <dt>주소</dt>
             <dd>주소 : 서울시 강남구 강남대로 00</dd>
-          </dt>
-          <dt>
+          </DescriptionItem>
+          <DescriptionItem>
+            <dt>사업자등록번호</dt>
             <dd>사업자 등록번호 : 000 0000 0000</dd>
-          </dt>
-          <dt>
-            <dd> whatehappen@gmail.com</dd>
-          </dt>
-        </dl>
-      </address>
-      <p>
-        Copyright 2024, whats-happns Team member All pictures cannot be copied
-        without permission
-      </p>
+          </DescriptionItem>
+          <DescriptionItem>
+            <dt>이메일</dt>
+            <dd>whatehappen@gmail.com</dd>
+          </DescriptionItem>
+        </DescriptionList>
+      </StyledAddress>
     </FooterContent>
   );
 }
+
+Footer.propTypes = {
+  title: PropTypes.string,
+  companyName: PropTypes.string,
+  representative: PropTypes.string,
+  address: PropTypes.string,
+  businessNumber: PropTypes.string,
+  email: PropTypes.string,
+  backgroundColor: PropTypes.string,
+  textColor: PropTypes.string,
+  titleFontSize: PropTypes.string,
+};
