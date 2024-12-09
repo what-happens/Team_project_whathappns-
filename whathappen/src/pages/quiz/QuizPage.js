0db1129f2f6f05ee5e300f8e50e3520b4b5abb5c
ddhsl <dnjs9199@naver.com>
2024-12-10 03:52:06 +0900

    💡퀴즈페이지 주석 제거

--- whathappen/src/pages/quiz/QuizPage.js
+++ whathappen/src/pages/quiz/QuizPage.js
@@ -1,20 +1,19 @@
 import { useState } from "react";
-import QuizLanding from "../quizLanding/quizLanding";
+import QuizLanding from "../quizLanding/QuizLanding";
 import Quiz from "./Quiz";
-import QuizResult from "../quizResult/quizResult";
+import QuizResult from "../quizResult/QuizResult";
 
 function QuizPage() {
   const [step, setStep] = useState(1);
 
   const handleNext = () => {
-    setStep((prev) => prev + 1); // step을 1 증가시키는 함수
+    setStep((prev) => prev + 1);
   };
 
   return (
     <div>
       {step === 1 && <QuizLanding onNext={handleNext} />}
       {step === 2 && <Quiz onNext={handleNext} />}{" "}
-      {/* Quiz 컴포넌트에 onNext 전달 */}
       {step === 3 && <QuizResult />}
     </div>
   );
