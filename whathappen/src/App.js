1f0039cc349152e8b84da2471ba1d8952dc4c50f
ddhsl <dnjs9199@naver.com>
2024-12-09 09:17:35 +0900

    🎨 라우팅 경로 수정

--- whathappen/src/App.js
+++ whathappen/src/App.js
@@ -5,9 +5,10 @@ import Login from "./pages/login/Login";
 import Join from "./pages/join/Join";
 import { Provider, useDispatch } from "react-redux";
 import store from "./redux/store/store";
-import QuizLanding from "./pages/quizLanding/quizLanding";
-import QuizResult from "./pages/quizResult/quizResult";
+// import QuizLanding from "./pages/quizLanding/QuizLanding";
+// import QuizResult from "./pages/quizResult/QuizResult";
 // import Quiz from "./pages/quiz/Quiz";
+import QuizPage from "./pages/quiz/QuizPage";
 import Exercise from "./pages/exercise/Exercise";
 import Study from "./pages/study/Study";
 import StudyFinish from "./pages/study/StudyFinish";
@@ -56,8 +57,9 @@ function App() {
           <Route path="/*" element={<NotFound />}></Route>
           <Route path="/login" element={<Login />}></Route>
           <Route path="/join" element={<Join />}></Route>
-          <Route path="/quiz-landing" element={<QuizLanding />} />
-          <Route path="/quiz" element={<QuizResult />} />
+          {/* <Route path="/quiz-landing" element={<QuizLanding />} />
+          <Route path="/quiz" element={<QuizResult />} /> */}
+          <Route path="/quizpage" element={<QuizPage />} />
           <Route path="/exercise" element={<Exercise />} />
           <Route path="/study" element={<Study />}></Route>
           <Route path="/study-finish" element={<StudyFinish />}></Route>
