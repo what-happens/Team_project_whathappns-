41792dd3713c64d65bda7b924ca631620132ace5
siwoon1602 <siwoon1602@gmail.com>
2024-12-10 16:50:50 +0900

    ✨ Redux PersistGate 추가

--- whathappen/src/index.js
+++ whathappen/src/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "firebase/firestore";
 import { Provider } from "react-redux";
-import { store } from "./redux/store/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store/store";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
