2cffa0e68cd41aba5bbf2b3c22e2c9192b1664d8
siwoon1602 <siwoon1602@gmail.com>
2024-12-10 15:20:33 +0900

    ✨ Login 인증상태관리 authslice 생성 , store설정 ,Provider 연결

--- whathappen/src/redux/store/store.js
+++ whathappen/src/redux/store/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../authSlice";
 
-const store = configureStore({
+export const store = configureStore({
   reducer: {
     auth: authReducer,
   },
 });
-
-export default store;
