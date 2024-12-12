d2a25347f9ed1a48a740faf3f8192a844a809d2a
siwoon1602 <siwoon1602@gmail.com>
2024-12-12 16:25:13 +0900

    ✨ Logout hook 으로 분리작업

---
+++ whathappen/src/hooks/useLogout.js
@@ -0,0 +1,33 @@
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase";
+import { logout as logoutAction } from "../redux/authSlice";
+
+export default function useLogout() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      dispatch(logoutAction());
+
+      const response = await fetch("http://localhost:5000/user/logout", {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.ok) {
+        navigate("/");
+      }
+    } catch (error) {
+      console.error("로그아웃불가:", error);
+    }
+  };
+
+  return { logout: handleLogout };
+}
