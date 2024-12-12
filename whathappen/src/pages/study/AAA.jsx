140fd869022899ab26a1063946e09590bcec6948
yejin.park <yejin5128@gmail.com>
2024-12-13 00:28:50 +0900

    🚚 목차 test 파일 위치 이동

--- whathappen/src/pages/study/AAA.jsx
+++ whathappen/src/pages/study/AAA.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-// import ToggleMenu from "./components/testN";
-// import ToggleMenu from "./components/NestedList";
-import ToggleMenu from "./components/NestedList";
-// import ToggleMenu from "./components/Test"; // 기본
-// import ToggleMenu from "./components/Toggle"; // 토글
-// import ToggleMenu from "./components/ToggleMenu";
+// import ToggleMenu from "./test/components/testN";
+// import ToggleMenu from "./test/components/NestedList";
+import ToggleMenu from "./test/NestedList copy";
+// import ToggleMenu from "./test/components/Test"; // 기본
+// import ToggleMenu from "./test/components/Toggle"; // 토글
+// import ToggleMenu from "./test/components/ToggleMenu";
 
 export default function AAA() {
   return <ToggleMenu>AAA</ToggleMenu>;
