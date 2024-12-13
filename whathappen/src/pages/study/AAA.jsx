0bbe1a022b81b256c4343917ed68289075699cd7
siwoon1602 <siwoon1602@gmail.com>
2024-12-13 17:42:31 +0900

    :poop: 테스트페이지 임시 삭제

--- whathappen/src/pages/study/AAA.jsx
+++ whathappen/src/pages/study/AAA.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-// import ToggleMenu from "./test/components/testN";
-// import ToggleMenu from "./test/components/NestedList";
-import ToggleMenu from "./test/NestedList";
-// import ToggleMenu from "./test/components/Test"; // 기본
-// import ToggleMenu from "./test/components/Toggle"; // 토글
-// import ToggleMenu from "./test/components/ToggleMenu";
+// import React from "react";
+// // import ToggleMenu from "./test/components/testN";
+// // import ToggleMenu from "./test/components/NestedList";
+// import ToggleMenu from "./test/NestedList";
+// // import ToggleMenu from "./test/components/Test"; // 기본
+// // import ToggleMenu from "./test/components/Toggle"; // 토글
+// // import ToggleMenu from "./test/components/ToggleMenu";
 
-export default function AAA() {
-  return <ToggleMenu>AAA</ToggleMenu>;
-}
+// export default function AAA() {
+//   return <ToggleMenu>AAA</ToggleMenu>;
+// }
