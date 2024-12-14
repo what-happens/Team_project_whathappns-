812a12023fefaf5758e486b2c169678714eaa60d
siwoon1602 <siwoon1602@gmail.com>
2024-12-14 16:08:44 +0900

    💄 마이페이지 ui 변경 및 디자인 수정정

--- whathappen/src/pages/myPage/MyPage.jsx
+++ whathappen/src/pages/myPage/MyPage.jsx
@@ -4,23 +4,124 @@ import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import Stamps from "./components/Stamps";
 import { media } from "../../styles/MideaQuery";
+import backGround from "../../assets/review-background-2.svg";
 
+export default function MyPage() {
+  return (
+    <Background>
+      <MyPageContents>
+        <header>
+          <h1 className="sr-only">마이페이지</h1>
+          <GreetingMsg>
+            <span className="greetings">안녕하세요, 오르미</span>
+            <span style={{ color: "white" }}> 고객님!</span>
+          </GreetingMsg>
+        </header>
+
+        <StatusContents>
+          <h2 className="sr-only">학습 현황</h2>
+          <StatusBox>
+            <StatusVal>1 stage</StatusVal>
+            <StatusLabel>기초학습</StatusLabel>
+          </StatusBox>
+          <Division />
+          <StatusBox>
+            <StatusVal>10회</StatusVal>
+            <StatusLabel>퀴즈풀이</StatusLabel>
+          </StatusBox>
+          <Division />
+          <StatusBox>
+            <StatusVal>29개</StatusVal>
+            <StatusLabel>저장된 퀴즈</StatusLabel>
+          </StatusBox>
+        </StatusContents>
+
+        <CourseContents>
+          <StampContents>
+            <h2>Stamp</h2>
+            <h2 className="sr-only">스탬프 북</h2>
+            <Stamps />
+          </StampContents>
+
+          <ProgressContents>
+            <h2 style={{ color: "var(--main-color)" }}>기초학습 진척도</h2>
+            <CircularProgressbar
+              value={20}
+              text={`20%`}
+              background={true}
+              styles={{
+                root: {
+                  maxHeight: "300px",
+                  maxWidth: "300px",
+                  minHeight: "220px",
+                  minWidth: "220px",
+                },
+                path: {
+                  stroke: `var(--main-color)`,
+                },
+                text: {
+                  fontSize: "1.5rem",
+                  fontWeight: 700,
+                  fill: "var(--main-color)",
+                },
+                background: {
+                  fill: "white",
+                },
+              }}
+            />
+          </ProgressContents>
+        </CourseContents>
+      </MyPageContents>
+    </Background>
+  );
+}
+
+const Background = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  min-height: 100vh;
+  z-index: -999;
+  background-image: url(${backGround});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  ${media.medium`
+    background-size: cover; 
+    height: 100%; 
+  `}
+
+  ${media.small`
+    background-size: cover; 
+    height: 100%; 
+  `}
+`;
 const MyPageContents = styled.div`
   display: flex;
   flex-direction: column;
-  height: 80vh;
-  width: 90vw;
-  margin: 4rem auto;
+  height: 60vh;
+  width: 80vw;
+  margin: 8rem auto;
   padding: 3rem 1rem 3rem 1rem;
   ${media.medium`
     margin-top: 1rem;
   `}
 `;
+
 const GreetingMsg = styled.div`
   font-size: 4.8rem;
   font-weight: 500;
   .greetings {
     font-weight: 900;
+    color: white;
   }
   br {
     display: none;
@@ -31,6 +132,7 @@ const GreetingMsg = styled.div`
     }
   `}
 `;
+
 const StatusContents = styled.div`
   display: flex;
   flex-direction: row;
@@ -40,13 +142,17 @@ const StatusContents = styled.div`
   padding: 2.4rem;
   width: 100%;
   height: 21.5rem;
-  box-shadow: rgba(0, 0, 0, 0.2) 0px 1px 10px 0px;
+  box-shadow:
+    rgba(9, 30, 66, 0.25) 0px 4px 8px -2px,
+    rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;
   margin: 3rem 0;
+  border-radius: 2rem;
 
   ${media.medium`
     min-width: 66rem;
-`}
+  `}
 `;
+
 const StatusBox = styled.div`
   font-size: 3.2rem;
   display: flex;
@@ -55,6 +161,7 @@ const StatusBox = styled.div`
   align-items: center;
   gap: 3rem;
 `;
+
 const StatusVal = styled.div`
   font-weight: 900;
 `;
@@ -64,109 +171,48 @@ const StatusLabel = styled.h3`
 `;
 
 const Division = styled.div`
-  height: 8.4rem;
-  width: rem;
+  height: 12.4rem;
+  width: 1px;
   background-color: #c4c4c4;
 `;
 
 const CourseContents = styled.section`
-  display: grid;
-  grid-template-areas: "a b";
-  gap: 3rem;
+  display: flex;
+  gap: 1rem;
+
   h2 {
     align-self: flex-start;
-    color: var(--main-color);
+    color: white;
     font-size: 4rem;
     font-weight: 700;
     margin-bottom: 1rem;
   }
+
   ${media.medium`
-    display: grid;
-    grid-template-areas: 
-    "a"
-    "b"
-    ;
+    flex-direction: column;
   `}
 `;
 
 const StampContents = styled.section`
-  grid-area: a;
+  flex: 1;
 `;
+
 const ProgressContents = styled.section`
-  grid-area: b;
+  flex: 0.5;
+  display: flex;
+  flex-direction: column;
+  border-radius: 2rem;
+  background-color: white;
+  box-shadow:
+    rgba(9, 30, 66, 0.25) 0px 4px 8px -2px,
+    rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;
   display: flex;
   justify-content: center;
   align-items: center;
-  margin-top: 3rem;
+
+  padding: 2rem;
+
+  ${media.medium`
+    min-width: 66rem;
+  `}
 `;
-
-export default function MyPage() {
-  return (
-    <MyPageContents>
-      <header>
-        <h1 className="sr-only">마이페이지</h1>
-        <GreetingMsg>
-          <span className="greetings">
-            안녕하세요, <br />
-            오르미
-          </span>
-          <span> 고객님</span>
-        </GreetingMsg>
-      </header>
-
-      <StatusContents>
-        <h2 className="sr-only">학습 현황</h2>
-        <StatusBox>
-          <StatusVal>1 stage</StatusVal>
-          <StatusLabel>기초학습</StatusLabel>
-        </StatusBox>
-        <Division />
-        <StatusBox>
-          <StatusVal>10회</StatusVal>
-          <StatusLabel>퀴즈풀이</StatusLabel>
-        </StatusBox>
-        <Division />
-        <StatusBox>
-          <StatusVal>29개</StatusVal>
-          <StatusLabel>저장된 퀴즈</StatusLabel>
-        </StatusBox>
-      </StatusContents>
-
-      <CourseContents>
-        <StampContents>
-          <h2>Course</h2>
-          <h2 className="sr-only">스탬프 북</h2>
-          <Stamps />
-        </StampContents>
-
-        <ProgressContents>
-          <h2 className="sr-only">학습 진척도</h2>
-          <CircularProgressbar
-            value={20}
-            text={`20%`}
-            background={true}
-            styles={{
-              root: {
-                maxHeight: "300px",
-                maxWidth: "300px",
-                minHeight: "220px",
-                minWidth: "220px",
-              },
-              path: {
-                stroke: `var(--main-color)`,
-              },
-              text: {
-                fontSize: "24px",
-                fontWeight: 700,
-                fill: "var(--main-color)",
-              },
-              background: {
-                fill: "#3E3E3E",
-              },
-            }}
-          />
-        </ProgressContents>
-      </CourseContents>
-    </MyPageContents>
-  );
-}
--- whathappen/src/pages/myPage/components/Stamps.jsx
+++ whathappen/src/pages/myPage/components/Stamps.jsx
@@ -3,23 +3,29 @@ import styled from "styled-components";
 import { media } from "../../../styles/MideaQuery";
 
 const StampBox = styled.div`
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-
-  background-color: var(--main-color);
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  gap: 2rem;
+  box-shadow:
+    rgba(9, 30, 66, 0.25) 0px 4px 8px -2px,
+    rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;
+  background-color: white;
   padding: 4rem 2rem 1rem 5rem;
-  width: 93rem;
+  width: 100rem;
   height: 429px;
   border-radius: 20px;
 
   ${media.medium`
     display: flex;
+    flex-wrap: nowrap;
     justify-content: space-between;
     align-items: center;
     padding: 2rem 1rem;
     width: 66rem;
     height: 11rem;
     border-radius: 1rem;
+    gap: 1rem;
   `}
 `;
 
@@ -28,15 +34,17 @@ const StampSlot = styled.div`
   border-radius: 1rem;
   width: 16.2rem;
   height: 16.2rem;
-  background-color: white;
+  background-color: var(--main-color);
   box-shadow: rgba(0, 0, 0, 0.15) 5px 5px 4px;
+
   ${media.medium`
     max-width: 7.213rem;
     height: 7.213rem;
     border-radius: 1rem;
-    margin: 0.9rem;
+    margin: 0;
   `}
 `;
+
 const Stamps = () => {
   return (
     <StampBox>
