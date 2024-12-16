9eafccbcc33a439caa358374e3cd627e5a26eac5
HwiGyoung Lee <ter0000@naver.com>
2024-12-16 10:31:53 +0900

    ✨ 퀴즈를 저장하는 기능 추가 및 명확성을 위해 fetchQuiz 에서 getQuiz로 변경경

--- whathappen/src/hooks/useFetchQuiz.js
+++ whathappen/src/hooks/useFetchQuiz.js
@@ -2,10 +2,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { setQuizData, setLoading } from "../redux/quizSlice";
 
 const useFetchQuiz = () => {
-  const { category, limit, isLoading } = useSelector((state) => state.quiz); // 선택된 카테고리와 제한 가져오기
+  const { category, limit, isLoading, incorrectQuiz } = useSelector(
+    (state) => state.quiz
+  ); // 선택된 카테고리와 제한 가져오기
   const dispatch = useDispatch();
 
-  const fetchQuiz = async () => {
+  const getQuiz = async () => {
     try {
       dispatch(setLoading(true)); // 로딩 시작
       const response = await fetch(
@@ -21,7 +23,29 @@ const useFetchQuiz = () => {
     }
   };
 
-  return { fetchQuiz, isLoading }; // 필요한 함수와 상태 반환
+  const postQuizResult = async () => {
+    try {
+      dispatch(setLoading(true)); // 로딩 시작
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/review`, {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          review: incorrectQuiz,
+        }),
+        credentials: "include",
+      });
+      const data = await response.json();
+      return data.review;
+    } catch (error) {
+      console.error(error.message);
+    } finally {
+      dispatch(setLoading(false)); // 로딩 종료
+    }
+  };
+
+  return { getQuiz, postQuizResult, isLoading }; // 필요한 함수와 상태 반환
 };
 
 export default useFetchQuiz;
