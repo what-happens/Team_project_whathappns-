cc11bf887174845fe0ffe6011b2d797b361d4af8
ddhsl <dnjs9199@naver.com>
2024-12-16 10:35:22 +0900

    💄 챗봇 모달 추가

--- whathappen/src/pages/Home.jsx
+++ whathappen/src/pages/Home.jsx
@@ -1,14 +1,39 @@
 import React from "react";
+import { useState } from "react";
+import styled from "styled-components";
 import InfiniteComponentLoader from "../components/InfiniteComponentLoader";
 import Header from "../components/Header";
+import Chatbot from "./chatbot/Chatbot";
+import chatOpen from "../assets/chatbot_open.png";
+import chatClose from "../assets/chatbot_close.png";
 
 const App = () => {
+  const [isChatbotOpen, setIsChatbotOpen] = useState(false);
+
+  const toggleChatbot = () => {
+    setIsChatbotOpen((prev) => !prev);
+  };
   return (
     <>
       <Header />
+      {isChatbotOpen && <Chatbot />}
       <InfiniteComponentLoader />
+      <ChatToggle
+        src={isChatbotOpen ? chatClose : chatOpen}
+        alt={isChatbotOpen ? "챗봇 닫기" : "챗봇 열기"}
+        onClick={toggleChatbot}
+      />
     </>
   );
 };
 
 export default App;
+
+const ChatToggle = styled.img`
+  width: 6rem;
+  height: 6rem;
+  position: fixed;
+  bottom: 1.8rem;
+  right: 2rem;
+  cursor: pointer;
+`;
