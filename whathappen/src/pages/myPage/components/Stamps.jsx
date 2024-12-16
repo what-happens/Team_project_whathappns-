6dbc48dbec8bc8ccf611ae7ed232b92497eabf3d
siwoon1602 <siwoon1602@gmail.com>
2024-12-16 10:08:37 +0900

    💄 마이페이지 StampSlot 반응형 스타일 추가가

--- whathappen/src/pages/myPage/components/Stamps.jsx
+++ whathappen/src/pages/myPage/components/Stamps.jsx
@@ -12,7 +12,7 @@ const StampBox = styled.div`
     rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;
   background-color: white;
   padding: 4rem 2rem 1rem 5rem;
-  width: 100rem;
+  width: 100%;
   height: 429px;
   border-radius: 20px;
 
@@ -22,7 +22,7 @@ const StampBox = styled.div`
     justify-content: space-between;
     align-items: center;
     padding: 2rem 1rem;
-    width: 66rem;
+    width: 100%;
     height: 11rem;
     border-radius: 1rem;
     gap: 1rem;
@@ -36,6 +36,14 @@ const StampSlot = styled.div`
   height: 16.2rem;
   background-color: var(--main-color);
   box-shadow: rgba(0, 0, 0, 0.15) 5px 5px 4px;
+  @media (max-width: 1280px) {
+    width: 12.2rem;
+    height: 12.2rem;
+  }
+  @media (max-width: 1020px) {
+    width: 10.2rem;
+    height: 10.2rem;
+  }
 
   ${media.medium`
     max-width: 7.213rem;
