d16abf9a71b24ae884f6a37273d8886e5ea9cb4a
HwiGyoung Lee <ter0000@naver.com>
2024-12-18 13:44:15 +0900

    ✨ user에게 답안 선택지를 보여주는 기능 추가

--- whathappen/src/pages/exercise/components/UserDisplay.jsx
+++ whathappen/src/pages/exercise/components/UserDisplay.jsx
@@ -1,10 +1,36 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import useExercise from "../../../hooks/useExercise";
+import BlankOption from "./BlankOption";
+import { useSelector } from "react-redux";
 
 export default function UserDisplay({ height }) {
+  const { selectedQid, questions, isShowAnswers } = useSelector(
+    (state) => state.learn
+  );
+  const { setIsShow, setSelectedUserAnswer, setExerciseSelectedQid } =
+    useExercise();
+  const handleOnClickBlankOption = (answer) => {
+    setIsShow(false);
+    setSelectedUserAnswer(answer);
+    setExerciseSelectedQid(null);
+  };
+
+  const selectedQuestion = questions.find(
+    (question) => question.q_id === selectedQid
+  );
+
   return (
-    <UserDisplayContainer $height={height}>UserDisplay</UserDisplayContainer>
+    <UserDisplayContainer $height={height}>
+      {isShowAnswers && (
+        <BlankOption
+          choices={selectedQuestion.choices}
+          q_id={selectedQuestion.q_id}
+          handleOnClick={handleOnClickBlankOption}
+        />
+      )}
+    </UserDisplayContainer>
   );
 }
 
