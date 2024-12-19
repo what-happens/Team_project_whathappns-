6b18f6be558c36257dec8edfb7336c82b24b5fc5
yejin.park <yejin5128@gmail.com>
2024-12-20 05:30:07 +0900

    🎨  App.js Study 라우팅 수정

--- whathappen/src/App.js
+++ whathappen/src/App.js
@@ -6,16 +6,12 @@ import Join from "./pages/join/Join";
 import QuizPage from "./pages/quiz/QuizPage";
 import Exercise from "./pages/exercise/Exercise";
 import Study from "./pages/study/Study";
-import StudyFinish from "./pages/study/StudyFinish";
-import LearningCourse from "./pages/study/LearningCourse";
-import LearningPage from "./pages/study/LearningPage";
 import NotFound from "./pages/notFound/NotFound";
 import MyPage from "./pages/myPage/MyPage";
 import Review from "./pages/review/ReviewFreeVersion";
 import AuthHeader from "./components/AuthHeader";
 import ProtectedRoute from "./components/ProtectedRoute";
 import JoinSuccess from "./pages/joinSuccess/JoinSuccess";
-import ParentPage from "./pages/study/p";
 
 function App() {
   return (
@@ -27,15 +23,11 @@ function App() {
         <Route path="/join" element={<Join />} />
         <Route path="/joinsuccess" element={<JoinSuccess />} />
         <Route path="/*" element={<NotFound />} />
-        <Route path="/aaa/*" element={<ParentPage />} />
+        <Route path="/study/*" element={<Study />} />
 
         <Route element={<ProtectedRoute />}>
           <Route path="/quizpage" element={<QuizPage />} />
           <Route path="/exercise/:stage/:level" element={<Exercise />} />
-          <Route path="/learncourse" element={<LearningCourse />} />
-          <Route path="/learnpage" element={<LearningPage />} />
-          <Route path="/study" element={<Study />} />
-          <Route path="/studyfinish" element={<StudyFinish />} />
 
           <Route element={<AuthHeader />}>
             <Route path="/mypage" element={<MyPage />} />
