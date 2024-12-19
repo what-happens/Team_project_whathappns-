01993f369283fc1ce1364274790a9f74b593c022
siwoon1602 <siwoon1602@gmail.com>
2024-12-19 17:37:25 +0900

    🎨 기존인증 로직 유지하면서 프로텍트 라우팅 중첩

--- whathappen/src/App.js
+++ whathappen/src/App.js
@@ -16,120 +16,34 @@ import Review from "./pages/review/ReviewFreeVersion";
 import AuthHeader from "./components/AuthHeader";
 import ProtectedRoute from "./components/ProtectedRoute";
 import JoinSuccess from "./pages/joinSuccess/JoinSuccess";
-// import { useEffect } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import { auth } from "./firebase";
-// import { login, logout } from "./redux/authSlice";
 
 function App() {
-  // const dispatch = useDispatch();
-  // const authState = useSelector((state) => state.auth);
-
-  // useEffect(() => {
-  //   const unsubscribe = auth.onAuthStateChanged((user) => {
-  //     if (user) {
-  //       dispatch(
-  //         login({
-  //           user: {
-  //             email: user.email,
-  //             displayName: user.displayName,
-  //           },
-  //         })
-  //       );
-  //     } else {
-  //       if (!authState.isLoggedIn) {
-  //         dispatch(logout());
-  //       }
-  //     }
-  //   });
-
-  //   console.log("Current auth state:", authState);
-
-  //   return () => unsubscribe();
-  // }, [dispatch, authState.isLoggedIn]);
-
   return (
     <BrowserRouter>
       <GlobalStyle />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/*" element={<NotFound />} />
         <Route path="/login" element={<Login />} />
         <Route path="/join" element={<Join />} />
-        <Route path="/Joinsuccess" element={<JoinSuccess />} />
-        <Route
-          path="/quizpage"
-          element={
-            // <ProtectedRoute>
-            <QuizPage />
-            // </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/exercise"
-          element={
-            // <ProtectedRoute>
-            <Exercise />
-            // </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/learncourse"
-          element={
-            // <ProtectedRoute>
-            <LearningCourse />
-            // </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/learnpage"
-          element={
-            // <ProtectedRoute>
-            <LearningPage />
-            // </ProtectedRoute>
-          }
-        />
-        {/* test 페이지 입니다...(예진) */}
+        <Route path="/joinsuccess" element={<JoinSuccess />} />
+        <Route path="/*" element={<NotFound />} />
         <Route path="/aaa" element={<AAA />} />
-        <Route
-          path="/study"
-          element={
-            // <ProtectedRoute>
-            <Study />
-            // </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/studyfinish"
-          element={
-            // <ProtectedRoute>
-            <StudyFinish />
-            // </ProtectedRoute>
-          }
-        />
 
-        <Route element={<AuthHeader />}>
-          <Route
-            path="/mypage"
-            element={
-              // <ProtectedRoute>
-              <MyPage />
-              // </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/review"
-            element={
-              <ProtectedRoute>
-                <Review />
-              </ProtectedRoute>
-            }
-          />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/quizpage" element={<QuizPage />} />
+          <Route path="/exercise" element={<Exercise />} />
+          <Route path="/learncourse" element={<LearningCourse />} />
+          <Route path="/learnpage" element={<LearningPage />} />
+          <Route path="/study" element={<Study />} />
+          <Route path="/studyfinish" element={<StudyFinish />} />
+
+          <Route element={<AuthHeader />}>
+            <Route path="/mypage" element={<MyPage />} />
+            <Route path="/review" element={<Review />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
   );
 }
-
 export default App;
