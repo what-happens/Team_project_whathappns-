633b84d9587a1a5b21d83f2a36a93c8e649ffef7
HwiGyoung Lee <ter0000@naver.com>
2024-12-19 11:03:40 +0900

    💄 CodeDisplayWrapper 적용

--- whathappen/src/pages/exercise/components/CodeDisplay.jsx
+++ whathappen/src/pages/exercise/components/CodeDisplay.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import BlankProblem from "./BlankProblem";
 import { useSelector } from "react-redux";
+import styled from "styled-components";
 
 export default function CodeDisplay() {
   const { parsedData } = useSelector((state) => state.learn);
 
   return (
-    <>
+    <CodeDisplayWrapper>
       {parsedData.length !== 0 &&
         parsedData.segments.map((segment, index) => {
           if (segment.type === "code") {
@@ -16,6 +17,19 @@ export default function CodeDisplay() {
           }
           return null;
         })}
-    </>
+    </CodeDisplayWrapper>
   );
 }
+
+const CodeDisplayWrapper = styled.section`
+  border: 1px solid var(--main-color);
+  flex: 1;
+  padding: 1rem;
+  height: calc(100% - 10rem);
+  pre {
+    margin: 0.5rem 0;
+    padding: 1rem;
+    font-size: 1.6rem;
+  }
+  border-bottom-left-radius: 2rem;
+`;
