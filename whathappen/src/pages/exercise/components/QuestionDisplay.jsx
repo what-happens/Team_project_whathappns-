c3e1d8c82a20718ef0f092231acbed02cad1a25f
HwiGyoung Lee <ter0000@naver.com>
2024-12-19 17:31:35 +0900

    💄 style 수정

--- whathappen/src/pages/exercise/components/QuestionDisplay.jsx
+++ whathappen/src/pages/exercise/components/QuestionDisplay.jsx
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import PropTypes from "prop-types";
 import { useSelector } from "react-redux";
 import Button from "../../../components/Button";
-export default function QuestionDisplay({ height }) {
+export default function QuestionDisplay() {
   const [isDisplayQuestion, setIsDisplayQuestion] = useState(true);
-  const { parsedData, selectedQid, questions, type, subcode } = useSelector(
+  const { parsedData, selectedQid, questions, type, subCode } = useSelector(
     (state) => state.learn
   );
 
@@ -20,12 +19,12 @@ export default function QuestionDisplay({ height }) {
   const displayQuestion = () => {
     if (selectedQid !== null) {
       return (
-        <p>
+        <Question>
           {questions.find((question) => question.q_id === selectedQid).q_title}
-        </p>
+        </Question>
       );
     }
-    return <p>문제를 보시려면 코드의 버튼을 눌러주세요</p>;
+    return <Question>문제를 보시려면 코드의 버튼을 눌러주세요</Question>;
   };
 
   const generateIframeContent = (cssCode, htmlCode) => {
@@ -47,52 +46,69 @@ export default function QuestionDisplay({ height }) {
   const displayCorrect = () => {
     let srcDoc = "";
     if (type === "css") {
-      srcDoc = generateIframeContent(parsedData.cssCode, subcode);
+      srcDoc = generateIframeContent(parsedData.cssCode, subCode);
     } else if (type === "html") {
-      srcDoc = generateIframeContent(subcode, parsedData.htmlCode);
+      srcDoc = generateIframeContent(subCode, parsedData.htmlCode);
     }
-    return <iframe srcDoc={srcDoc} width={600} height={400} />;
+    return <StyledIframe srcDoc={srcDoc} />;
   };
 
   return (
-    <QuestionContainer $height={height}>
-      <div>
+    <QuestionContainer>
+      <ButtonWrapper>
         <Button onClick={onClickDisplayQuestion}>문제 보기</Button>
         <Button onClick={onClickDisplayAnswer}>정답 화면 보기</Button>
-      </div>
-      {isDisplayQuestion ? displayQuestion() : displayCorrect()}
+      </ButtonWrapper>
+      <Content>
+        {isDisplayQuestion ? displayQuestion() : displayCorrect()}
+      </Content>
     </QuestionContainer>
   );
 }
 
 const QuestionContainer = styled.section`
   width: 100%;
-  height: 50%;
+  height: 100%;
   border: 1px solid var(--main-color);
-  border-radius: 0px 20px 0px 0px;
-  padding: 2rem;
-  overflow: auto;
-  div {
-    display: flex;
-    gap: 1rem;
-    margin-bottom: 2rem;
-  }
-  p {
-    margin: 1rem 0;
-    font-size: 1.6rem;
-    line-height: 1.5;
-  }
-  iframe {
-    border: none;
-    border-radius: 8px;
-  }
-  Button {
-    font-size: 2rem;
-    border-radius: 1rem;
-    padding: 1rem 3rem;
-  }
+  border-bottom-right-radius: 2rem;
 `;
 
-QuestionDisplay.propTypes = {
-  height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-};
+const ButtonWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+  height: 8rem;
+  padding: 2rem;
+`;
+
+const Content = styled.div`
+  width: 100%;
+  height: calc(100% - 8rem);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Question = styled.p`
+  font-size: 2.4rem;
+  line-height: 2.4rem;
+  word-break: keep-all;
+`;
+const StyledIframe = styled.iframe`
+  width: 100%;
+  height: 90%;
+  /* 웹킷 기반 브라우저 */
+  &::-webkit-scrollbar {
+    width: 8px;
+  }
+  &::-webkit-scrollbar-track {
+    background: #f0f0f0;
+  }
+  &::-webkit-scrollbar-thumb {
+    background: #888;
+    border-radius: 10px;
+  }
+  &::-webkit-scrollbar-thumb:hover {
+    background: #555;
+  }
+`;
